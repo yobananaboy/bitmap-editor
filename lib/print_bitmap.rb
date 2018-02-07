f6e80b93247d0a01c311180a913ef8845e6915e0
Matt <mattkeegan@btinternet.com>
2018-02-07 17:41:05 +0000

    Test showing bitmap. Update test for printing bitmap

---
+++ lib/print_bitmap.rb
@@ -0,0 +1,2 @@
+def printBitmap(bitmap)
+end
--- spec/print_bitmap_spec.rb
+++ spec/print_bitmap_spec.rb
@@ -5,7 +5,7 @@ describe "prints bitmap" do
        it "prints 2 by 2 empty bitmap" do
            bitmap = {"1,1" => "O", "1,2" => "O", "2,1" => "O", "2,2" => "O"}
            expected_output = "OO\nOO"
-           expect(createEmptyBitmap(bitmap).should_receive(:puts).with(expected_output))
+           expect(printBitmap(bitmap)).to output(expected_output).to_stdout
        end
     end
 
