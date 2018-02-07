902f71bab7c05b32361f096d137edad73c0df0ad
Matt <mattkeegan@btinternet.com>
2018-02-07 21:20:48 +0000

    Colour multiple test passes

--- lib/colour_multiple_pixels.rb
+++ lib/colour_multiple_pixels.rb
@@ -1,2 +1,12 @@
-def colourMultiplePixels
+def colourMultiplePixels(bitmap, x, y, colour)
+    if x.is_a? Array
+        for i in x[0]..x[1]
+            bitmap["#{i},#{y}"] = colour
+        end
+    else
+        for i in y[0]..y[1]
+            bitmap["#{x},#{i}"] = colour
+        end
+    end
+    bitmap
 end
