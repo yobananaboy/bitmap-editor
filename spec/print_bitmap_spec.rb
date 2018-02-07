d3eca8510695d37d1e35861096fb75b48fe1e858
Matt <mattkeegan@btinternet.com>
2018-02-07 20:40:36 +0000

    Test bitmap printer. Refactor test

--- spec/print_bitmap_spec.rb
+++ spec/print_bitmap_spec.rb
@@ -1,12 +1,5 @@
 require('print_bitmap')
 
-printBitmap({
-               "1,1" => "O", "1,2" => "O", "1,3" => "O", "1,4" => "O", "1,5" => "O",
-               "2,1" => "O", "2,2" => "O", "2,3" => "O", "2,4" => "O", "2,5" => "O",
-               "3,1" => "O", "3,2" => "O", "3,3" => "O", "3,4" => "O", "3,5" => "O",
-               "4,1" => "O", "4,2" => "O", "4,3" => "O", "4,4" => "O", "4,5" => "O"
-           }, 5, 4)
-
 describe "prints bitmap" do
     context "given print command and bitmap, bitmap width and bitmap height to print" do
        it "prints 2 by 2 empty bitmap" do
@@ -24,6 +17,21 @@ describe "prints bitmap" do
                "1,4" => "O", "2,4" => "O", "3,4" => "O", "4,4" => "O", "5,4" => "O"
            }
            expect { printBitmap(bitmap, 5, 4) }.to output("OOOOO\nOOOOO\nOOOOO\nOOOOO\n").to_stdout
+       end
+    end
+    
+    context "given print command and bitmap, bitmap width and bitmap height to print, mixing up the colours" do
+       it "prints 3 by 7 empty bitmap" do
+           bitmap = {
+               "1,1" => "C", "2,1" => "C", "3,1" => "O",
+               "1,2" => "O", "2,2" => "O", "3,2" => "O",
+               "1,3" => "O", "2,3" => "O", "3,3" => "O",
+               "1,4" => "D", "2,4" => "Z", "3,4" => "O",
+               "1,5" => "O", "2,5" => "Z", "3,5" => "O",
+               "1,6" => "O", "2,6" => "Z", "3,6" => "O",
+               "1,7" => "O", "2,7" => "O", "3,7" => "O"
+           }
+           expect { printBitmap(bitmap, 5, 4) }.to output("CCO\nOOO\nOOO\nDZO\nZO\nOZO\nOOO\n").to_stdout
        end
     end
 end
