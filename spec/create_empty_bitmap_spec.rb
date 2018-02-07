1a2fc669f4cc01efad0333a68d77cce5760f3c09
Matt <mattkeegan@btinternet.com>
2018-02-07 17:29:56 +0000

    Test creating empty bitmap. Refactor test for maxiumum grid size

--- spec/create_empty_bitmap_spec.rb
+++ spec/create_empty_bitmap_spec.rb
@@ -1,12 +1,23 @@
 require 'create_empty_bitmap'
 
 describe "create empty bitmap" do
-    context "given 'I 2 3'" do
+    context "given 'empty bitmap and width of 2 and height of 3'" do
        it "returns empty 2 by 3 bitmap grid" do
            bitmap = Hash.new("O")
            expect(createEmptyBitmap(bitmap, 2, 3)).to include("2,3" => "O")
            expect(createEmptyBitmap(bitmap, 2, 3)).to include("1,1" => "O")
        end
     end
+    
+    context "given 'empty bitmap and width of 250 and height of 250'" do
+        it "returns empty 250 by 250 bitmap grid" do
+            bitmap = Hash.new("O")
+            expect(createEmptyBitmap(bitmap, 250, 250)).to include("250,250" => "O")
+            expect(createEmptyBitmap(bitmap, 250, 250)).to include("150,170" => "O")
+            expect(createEmptyBitmap(bitmap, 250, 250)).not_to include("251,251")
+            expect(createEmptyBitmap(bitmap, 250, 250)).not_to include("0,0")
+        end
+    end
+    
 
 end
