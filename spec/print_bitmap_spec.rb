7b8b37a947a458c5d99e688187b616efe3a1598e
Matt <mattkeegan@btinternet.com>
2018-02-07 20:36:23 +0000

    Refactor print bitmap test. Passes

--- spec/print_bitmap_spec.rb
+++ spec/print_bitmap_spec.rb
@@ -10,7 +10,7 @@ printBitmap({
 describe "prints bitmap" do
     context "given print command and bitmap, bitmap width and bitmap height to print" do
        it "prints 2 by 2 empty bitmap" do
-           bitmap = {"1,1" => "O", "1,2" => "O", "2,1" => "O", "2,2" => "O"}
+           bitmap = {"1,1" => "O", "2,1" => "O", "1,2" => "O", "2,2" => "O"}
            expect { printBitmap(bitmap, 2, 2) }.to output("OO\nOO\n").to_stdout
        end
     end
@@ -18,10 +18,10 @@ describe "prints bitmap" do
     context "given print command and bitmap, bitmap width and bitmap height to print" do
        it "prints 5 by 4 empty bitmap" do
            bitmap = {
-               "1,1" => "O", "1,2" => "O", "1,3" => "O", "1,4" => "O", "1,5" => "O",
-               "2,1" => "O", "2,2" => "O", "2,3" => "O", "2,4" => "O", "2,5" => "O",
-               "3,1" => "O", "3,2" => "O", "3,3" => "O", "3,4" => "O", "3,5" => "O",
-               "4,1" => "O", "4,2" => "O", "4,3" => "O", "4,4" => "O", "4,5" => "O"
+               "1,1" => "O", "2,1" => "O", "3,1" => "O", "4,1" => "O", "5,1" => "O",
+               "1,2" => "O", "2,2" => "O", "3,2" => "O", "4,2" => "O", "5,2" => "O",
+               "1,3" => "O", "2,3" => "O", "3,3" => "O", "4,3" => "O", "5,3" => "O",
+               "1,4" => "O", "2,4" => "O", "3,4" => "O", "4,4" => "O", "5,4" => "O"
            }
            expect { printBitmap(bitmap, 5, 4) }.to output("OOOOO\nOOOOO\nOOOOO\nOOOOO\n").to_stdout
        end
