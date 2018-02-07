65f518e44d98d5a928eb9cde1eb3282d3b869f6f
Matt <mattkeegan@btinternet.com>
2018-02-07 12:05:37 +0000

    Test input validator. Refactor test for colouring individual pixel

--- spec/validate_input_spec.rb
+++ spec/validate_input_spec.rb
@@ -30,4 +30,23 @@ describe "validate input" do
             expect(validateInput("I 300 0")).to eq false
         end
     end
+    
+    context "given 'L 1 2 C" do
+        it "returns true" do
+           expect(validateInput("L 1 2 C")).to eq true
+        end
+    end
+    
+    context "given 'L 0 0 C'" do
+        it "returns false" do
+            expect(validateInput("L 0 0 C"))
+        end
+    end
+    
+    context "given 'L 50 50 C' and pigment coordinates out of bitmap range" do
+        it "returns false" do
+            expect(validateInput("L 50 50 C", 30, 30)).to eq false
+        end
+    end
+    
 end
